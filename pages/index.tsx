fc1d480ede184301b82f1a2d3dfc003bc31673be
Jose L. Escalona <jlescalonap@gmail.com>
2022-11-11 14:42:45 -0300

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { MainLayout } from '../components/layouts/MainLayout'
 export default function HomePage() {
   return (
     <MainLayout>
-      <h1>Faz o L</h1>
+      <h1>Home Page</h1>
 
       <h1 className={'title'}>
         Ir a <Link href="/about">About</Link>
